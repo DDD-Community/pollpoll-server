e02fcb7cfb38c33f5f2d4675ee1ada2b8f985e44
Junhong-Kim <jhk1005jhk@gmail.com>
2022-12-21 00:07:36 +0900

    feat: Swagger 설정 추가

---
+++ pollpoll-api/src/main/kotlin/com/ddd/polllpoll/config/SwaggerConfig.kt
@@ -0,0 +1,24 @@
+package com.ddd.polllpoll.config
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.servers.Server
+import org.springdoc.core.GroupedOpenApi
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@OpenAPIDefinition(
+    servers = [Server(url = "/")],
+    info = Info(title = "폴폴 API", version = "v1")
+)
+@Configuration
+class SwaggerConfig {
+
+    @Bean
+    fun groupedOpenApi(): GroupedOpenApi {
+        return GroupedOpenApi.builder()
+            .group("pollpoll-api")
+            .pathsToMatch("/api/**")
+            .build()
+    }
+}

272f2775e7f085354685005e8f13598bf1ef7522
Junhong-Kim <jhk1005jhk@gmail.com>
2022-12-26 02:57:03 +0900

    feat: service layer 위치 이동

--- pollpoll-api/build.gradle.kts
+++ pollpoll-api/build.gradle.kts
@@ -7,10 +7,15 @@ tasks.getByName("jar") {
 }
 
 dependencies {
+    // module
     implementation(project(":pollpoll-core"))
     implementation(project(":storage:db-pollpoll"))
     implementation(project(":support:logging"))
 
+    // spring
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // etc
     implementation("org.springdoc:springdoc-openapi-ui:1.6.14")
 }

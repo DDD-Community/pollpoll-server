ca374e5f73c839566d2c979c0d8c3388ac2ece13
Junhong-Kim <jhk1005jhk@gmail.com>
2023-01-02 00:02:44 +0900

    feat: 개발 DB 설정 추가

--- storage/db-pollpoll/build.gradle.kts
+++ storage/db-pollpoll/build.gradle.kts
@@ -2,5 +2,7 @@ dependencies {
     implementation(project(":pollpoll-core"))
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    runtimeOnly("mysql:mysql-connector-java")
     runtimeOnly("com.h2database:h2")
 }

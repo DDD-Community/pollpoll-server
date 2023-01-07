c5cf4087150cd2d6b719a3800ec9b1cbf9c043c2
Junhong-Kim <jhk1005jhk@gmail.com>
2023-01-07 17:06:35 +0900

    feat: Swagger BearerAuth 설정 추가

--- pollpoll-api/src/main/kotlin/com/ddd/pollpoll/config/SwaggerConfig.kt
+++ pollpoll-api/src/main/kotlin/com/ddd/pollpoll/config/SwaggerConfig.kt
@@ -3,9 +3,14 @@ package com.ddd.pollpoll.config
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import io.swagger.v3.oas.annotations.info.Info
 import io.swagger.v3.oas.annotations.servers.Server
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springdoc.core.GroupedOpenApi
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders
 
 @OpenAPIDefinition(
     servers = [Server(url = "/")],
@@ -13,6 +18,22 @@ import org.springframework.context.annotation.Configuration
 )
 @Configuration
 class SwaggerConfig {
+
+    @Bean
+    fun openApi(): OpenAPI {
+        val securityScheme = SecurityScheme()
+            .type(SecurityScheme.Type.HTTP)
+            .scheme("Bearer")
+            .bearerFormat("JWT")
+            .`in`(SecurityScheme.In.HEADER)
+            .name(HttpHeaders.AUTHORIZATION)
+        val securityRequirement = SecurityRequirement()
+            .addList("BearerAuth")
+
+        return OpenAPI()
+            .components(Components().addSecuritySchemes("BearerAuth", securityScheme))
+            .security(listOf(securityRequirement))
+    }
 
     @Bean
     fun groupedOpenApi(): GroupedOpenApi {
